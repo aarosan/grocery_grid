ef65880e32ff207f6aba4a317508db72db890380
aaron-ross-sanchez <147529595+aaron-ross-sanchez@users.noreply.github.com>
2024-08-14 10:53:04 -0500

    Updated Meal.js to store mealTypes as an array of strings, instead of a single string

--- server/models/Meal.js
+++ server/models/Meal.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require('mongoose');
 
 const MealSchema = new Schema({
-  mealType: { type: String, required: true }, // e.g., Breakfast, Lunch, Dinner
+  mealType: { type: [String], required: true }, // e.g., Breakfast, Lunch, Dinner
   mealName: { type: String, required: true },
   ingredients: { type: [String], required: true }, // List of ingredients as strings
   user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
