711c39994ef568b96b2ab84ae20dc06f004ae8e6
aaron-ross-sanchez <147529595+aaron-ross-sanchez@users.noreply.github.com>
2024-08-14 10:49:17 -0500

    Refactored server/routes/api/index.js to only contain needed routes

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -2,12 +2,8 @@
 
 const router = require('express').Router();
 
-// import routes/api/testRoutes
-const testRoutes = require('./testRoutes');
 const userRoutes = require('./userRoutes');
 
-// URL is now http://localhost:3001/api/tests
-router.use('/tests', testRoutes);
 router.use('/users', userRoutes);
 
 module.exports = router;
