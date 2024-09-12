b1e0147932e74120dfe2dfabb66c9ec3e7385433
aaron-ross-sanchez <147529595+aaron-ross-sanchez@users.noreply.github.com>
2024-09-12 18:26:48 -0500

    Debug Test

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 require('dotenv').config(); 
 
 const dbUri = process.env.MONGODB_URI;
+console.log('dbUri:', dbUri);
+
+if (!dbUri) {
+  throw new Error('MONGODB_URI environment variable is not set.');
+}
 
 mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
