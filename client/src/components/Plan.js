f2444095af15a8fa688da97ca31cf6d26d919e4c
aaron-ross-sanchez <147529595+aaron-ross-sanchez@users.noreply.github.com>
2024-09-12 16:39:36 -0500

    Added Plan.js to this push

---
+++ client/src/components/Plan.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Plan = () => {
+  return (
+    <div>
+      <h1>Welcome to the Meal Planning Page</h1>
+      {/* Add content for the Page component here */}
+    </div>
+  );
+};
+
+export default Plan;
