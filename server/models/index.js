2f97ad3f0a6ced30dab09acfcd337506ed97a1d1
aaron-ross-sanchez <147529595+aaron-ross-sanchez@users.noreply.github.com>
2024-10-07 17:43:22 -0500

    Cleanup

--- server/models/index.js
+++ server/models/index.js
@@ -1,8 +1,4 @@
-// This index.js is the entrance point for the models directory
-// All of the models are imported and then exported all together
-
-const Test = require('./test');
 const User = require('./User');
 const Meal = require('./Meal')
 
-module.exports = { Test, User, Meal};
+module.exports = { User, Meal };
